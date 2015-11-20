fc3e91adb7c05cade942a667fb8d527af6e0423f
Robert Mischke <robert.mischke@dlr.de>
2015-11-20 11:21:58 +0100

    added first draft of a script to automatically install the signing key and repositories for RCE 7.x on Debian-based systems

---
+++ register_rce_7.x_deb_repository.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+echo Downloading key...
+# note: the same key is used for 6.x and 7.x releases
+rm -f rce_6.x_signing_key.asc
+wget -q https://raw.githubusercontent.com/rcenvironment/rce-signing/master/rce_6.x_signing_key.asc
+# TODO add error code checking
+echo Done.
+echo
+echo The sequence of numbers and letters below is the so-called \"fingerprint\" 
+echo of the signature used to verify RCE releases before installing them. You 
+echo should compare it against the RCE team\'s announcements, and only proceed
+echo if it matches.
+gpg --with-fingerprint rce_6.x_signing_key.asc | grep fingerprint
+
+while true; do
+    read -p "Proceed [y/n]? " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer with 'y' or 'n'";;
+    esac
+done
+
+apt-key adv --import rce_6.x_signing_key.asc
+echo Installing the RCE repository into the package management\'s list of sources
+rm -f /etc/apt/sources.list.d/dlr_rce_7_releases.list
+rm -f /etc/apt/sources.list.d/dlr_rce_7_snapshots.list
+# TODO query for this choice
+#echo "deb http://software.dlr.de/updates/rce/7.x/products/standard/snapshots/trunk/deb/ /" >/etc/apt/sources.list.d/dlr_rce_7_snapshots.list
+echo "deb http://software.dlr.de/updates/rce/7.x/products/standard/releases/latest/deb/ /" >/etc/apt/sources.list.d/dlr_rce_7_releases.list
+
+# TODO add error code checking
+echo 
+echo If no error messages appeared above, the signature and download locations
+echo for RCE 7.x have been successfully installed in your system. You can use 
+echo the standard tools of your operating system to install and update the 
+echo \"rce\" software package. You will also receive automatic notifications
+echo on updates.
+echo
+
+while true; do
+    read -p "Would you like to install RCE automatically now [y/n]? " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer with 'y' or 'n'";;
+    esac
+done
+
+echo Updating software repositories...
+apt-get update
+echo Installing RCE
+apt-get install rce
