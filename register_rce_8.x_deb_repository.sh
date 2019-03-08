696460777119eeb2e3390a5963febed462ae2648
Alexander Weinert <alexander.weinert@dlr.de>
2019-03-08 16:03:04 +0100

    Replace scripts by stubs pending update

--- register_rce_8.x_deb_repository.sh
+++ register_rce_8.x_deb_repository.sh
@@ -1,57 +1,15 @@
 #!/bin/bash
 
-KEY_EXPORT_FILE_NAME=rce_9.x_signing_key.asc
-
-echo Downloading key...
-rm -f "${KEY_EXPORT_FILE_NAME}"
-wget -q "https://raw.githubusercontent.com/rcenvironment/rce-signing/master/${KEY_EXPORT_FILE_NAME}"
-# TODO add error code checking
-echo Done.
-echo
-echo The sequence of numbers and letters below is the so-called \"fingerprint\" 
-echo of the signature used to verify RCE releases before installing them. You 
-echo should compare it against the RCE team\'s announcements, and only proceed
-echo if it matches.
-gpg --with-fingerprint "${KEY_EXPORT_FILE_NAME}" | grep fingerprint
-
-while true; do
-    read -p "Proceed [y/n]? " yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer with 'y' or 'n'";;
-    esac
-done
-
-apt-key adv --import "${KEY_EXPORT_FILE_NAME}"
-echo Installing the RCE repository into the package management\'s list of sources
-rm -f /etc/apt/sources.list.d/dlr_rce_7_releases.list
-rm -f /etc/apt/sources.list.d/dlr_rce_7_snapshots.list
-rm -f /etc/apt/sources.list.d/dlr_rce_8_releases.list
-rm -f /etc/apt/sources.list.d/dlr_rce_8_snapshots.list
-# TODO query for this choice
-#echo "deb https://software.dlr.de/updates/rce/8.x/products/standard/snapshots/trunk/deb/ /" >/etc/apt/sources.list.d/dlr_rce_8_snapshots.list
-echo "deb https://software.dlr.de/updates/rce/8.x/products/standard/releases/latest/deb/ /" >/etc/apt/sources.list.d/dlr_rce_8_releases.list
-
-# TODO add error code checking
-echo 
-echo If no error messages appeared above, the signature and download locations
-echo for RCE 8.x have been successfully installed in your system. You can use 
-echo the standard tools of your operating system to install and update the 
-echo \"rce\" software package. You will also receive automatic notifications
-echo on updates.
-echo
-
-while true; do
-    read -p "Would you like to install RCE automatically now [y/n]? " yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer with 'y' or 'n'";;
-    esac
-done
-
-echo Updating software repositories...
-apt-get update
-echo Installing RCE
-apt-get install rce
+echo "These scripts are currently being improved and adapted to our new workflow for"
+echo "producing signed RCE releases. They will be updated once they are ready for use"
+echo "in production."
+echo ""
+echo "In the meantime, please import the key with the following fingerprint manually:"
+echo ""
+echo ""
+echo "  258B C129 EDA2 389D 3ECD 2DE6 BA88 0CB3 9DC1 CE34"
+echo ""
+echo "You can find this key either in this repository in the file"
+echo "`rce_9.x_signing_key.asc` or on the keyserver `pool.sks-keyservers.net`."
+echo "For more information, please refer to the documentation of your system or to"
+echo "your system administrator."
--- register_rce_9.x_gpg_key.sh
+++ register_rce_9.x_gpg_key.sh
@@ -2,32 +2,16 @@
 
 KEY_EXPORT_FILE_NAME=rce_9.x_signing_key.asc
 
-echo Downloading key...
-rm -f "${KEY_EXPORT_FILE_NAME}"
-wget -q "https://raw.githubusercontent.com/rcenvironment/rce-signing/master/${KEY_EXPORT_FILE_NAME}"
-# TODO add error code checking
-echo Done.
-echo
-echo The sequence of numbers and letters below is the so-called \"fingerprint\" 
-echo of the signature used to verify RCE releases before installing them. You 
-echo should compare it against the RCE team\'s announcements, and only proceed
-echo if it matches.
-gpg --with-fingerprint "${KEY_EXPORT_FILE_NAME}" | grep fingerprint
-
-while true; do
-    read -p "Proceed [y/n]? " yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer with 'y' or 'n'";;
-    esac
-done
-
-gpg --import "${KEY_EXPORT_FILE_NAME}"
-# TODO add error code checking
-
-echo 
-echo If no error messages appeared above, the signature for RCE 8.x and 9.x releases
-echo has been successfully installed in your system. You can use this to verify
-echo downloaded installation files using command-line tools\; please refer
-echo to the RCE User Guide for details.
+echo "These scripts are currently being improved and adapted to our new workflow for"
+echo "producing signed RCE releases. They will be updated once they are ready for use"
+echo "in production."
+echo ""
+echo "In the meantime, please import the key with the following fingerprint manually:"
+echo ""
+echo ""
+echo "  258B C129 EDA2 389D 3ECD 2DE6 BA88 0CB3 9DC1 CE34"
+echo ""
+echo "You can find this key either in this repository in the file"
+echo "`rce_9.x_signing_key.asc` or on the keyserver `pool.sks-keyservers.net`."
+echo "For more information, please refer to the documentation of your system or to"
+echo "your system administrator."
