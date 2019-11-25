6fb8da1b680ba130343250790eb1d89db2bbb28e
Alexander Weinert <alexander.weinert@dlr.de>
2019-11-25 15:15:59 +0100

    Commit signing keys for RCE 10.0.0

---
+++ register_rce_10.x_gpg_key.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+KEY_EXPORT_FILE_NAME=rce_signing_key.asc
+
+echo "These scripts are currently being improved and adapted to our new workflow for"
+echo "producing signed RCE releases. They will be updated once they are ready for use"
+echo "in production."
+echo ""
+echo "In the meantime, please import the key with the following fingerprint manually:"
+echo ""
+echo ""
+echo "  258B C129 EDA2 389D 3ECD 2DE6 BA88 0CB3 9DC1 CE34"
+echo ""
+echo "You can find this key either in this repository in the file"
+echo "`rce_signing_key.asc` or on the keyserver `pool.sks-keyservers.net`."
+echo "For more information, please refer to the documentation of your system or to"
+echo "your system administrator."
